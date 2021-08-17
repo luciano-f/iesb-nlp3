b2c46ffc41dac3ef1b1b8c2d8ab6bcc90f12c4f9
me <me@home>
2021-08-17 00:12:35 -0300

    Modelo com rede convolucional surgindo, mas houve problema com o treino da segunda epoca com deepcopy

--- trabalho/metodos.py
+++ trabalho/metodos.py
@@ -19,10 +19,10 @@ from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Input
+from tensorflow.keras.models import Sequential, Model
 from tensorflow.keras.losses import BinaryCrossentropy
 from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
+from tensorflow.keras.layers import Dense, Input, Conv1D, MaxPooling1D, Flatten
 
 from string import punctuation, whitespace
 
@@ -155,7 +155,7 @@ class Metodo3NeuralNetwork(MetodoGeral):
         self.clf = MLPClassifier((500, 300, 100, 30))
 
 
-class Metodo4NeuralNetworkEspecializada(MetodoGeral):
+class Metodo4NeuralNetworkKerasMLP(MetodoGeral):
     def __init__(self, x_set, y_set, seed=123):
         super().__init__(x_set, y_set, seed=seed)
 
@@ -178,7 +178,7 @@ class Metodo4NeuralNetworkEspecializada(MetodoGeral):
                              mode='auto')
         self.callbacks = [es, mc]
         self.BATCH_SIZE = 10
-        self.EPOCHS = 50
+        self.EPOCHS = 1
 
         self.clf = None
         self.hist = None
@@ -261,18 +261,76 @@ class Metodo4NeuralNetworkEspecializada(MetodoGeral):
         return pred
 
 
+class Metodo5NNeuralNetworkEspecializada(Metodo4NeuralNetworkKerasMLP):
 
+    def set_up_model(self):
+        # Referência: https://medium.com/@mayankverma05032001/binary-classification-using-convolution-neural-network-cnn-model-2635ddcdc510
+        # https://stackoverflow.com/questions/66718335/input-0-of-layer-conv1d-is-incompatible-with-the-layer-expected-min-ndim-3-f
+        super().set_up_model()
+
+        # https://stackoverflow.com/questions/66718335/input-0-of-layer-conv1d-is-incompatible-with-the-layer-expected-min-ndim-3-f
+        entrada = Input(shape=(self.X_train.shape[1],1), name='entrada')
+        c1 = Conv1D(32, 3, strides=2, activation='relu', input_shape=[None, entrada])(entrada)
+        m1 = MaxPooling1D(pool_size=3)(c1)
+        c2 = Conv1D(16, 3, strides=2, activation='relu', input_shape=[None, entrada])(m1)
+        m2 = MaxPooling1D(pool_size=3)(c2)
+        f1 = Flatten()(m2)
+        d1 = Dense(units=500, activation='relu')(f1)
+        saida = Dense(units=1, activation='sigmoid')(d1)
+
+        model = Model(inputs=entrada, outputs=saida)
+        model.summary()
+
+        model.compile(loss=BinaryCrossentropy(from_logits=True),
+                      optimizer='adam',
+                      metrics=['accuracy'])
+
+        self.clf = model
+
+        def data_generator(self, x_set=None, y_set=None):
+            # Erro ao passar matriz esparsa 'SparseTensor' object is not subscriptable
+            # https://stackoverflow.com/questions/53779968/why-keras-fit-generator-load-before-actually-training
+            x_set = self.X_train if x_set is None else x_set
+            y_set = self.y_train if y_set is None else y_set
+
+            iter_x = iter(copy.deepcopy(x_set))
+            iter_y = iter(copy.deepcopy(y_set))
+            while True:
+                # obs: esse método precisa ser chamado após X_train receber a transformação de espaço para tdidf
+                x = np.zeros((self.BATCH_SIZE, x_set.shape[1]))
+                y = np.zeros(self.BATCH_SIZE)
+                for i in range(self.BATCH_SIZE):
+                    try:
+                        x[i] = next(iter_x).toarray()
+                        y[i] = next(iter_y)
+                    except StopIteration:
+                        iter_x = iter(copy.deepcopy(x_set))
+                        iter_y = iter(copy.deepcopy(y_set))
+                yield x, y
+
+        def predict_gen(self):
+            iter_x = iter(copy.deepcopy(self.X_test))
+            while True:
+                # obs: esse método precisa ser chamado após X_train receber a transformação de espaço para tdidf
+                x = np.zeros((self.BATCH_SIZE, self.X_test.shape[1]))
+                for i in range(self.BATCH_SIZE):
+                    try:
+                        x[i] = next(iter_x).toarray()
+                    except StopIteration:
+                        return
+                    yield x
 
 # Resultados
 
 
 def rotina_m4():
     """Rotina de configuração de uma instância m4"""
-    m4 = Metodo4NeuralNetworkEspecializada(X_orig, y_orig)
+    m4 = Metodo4NeuralNetworkKerasMLP(X_orig, y_orig)
     m4.set_up_model()
     m4.transform_test()
     m4.treinar()
     return m4
+
 
 def gerar_indicadores(y_true, y_pred):
     f1 = f1_score(y_true, y_pred, pos_label=4)
